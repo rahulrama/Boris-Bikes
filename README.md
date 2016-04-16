d898325d24ad7e06a0d83b83cddc22bd06be363b
rahulrama <rahulrama@gmail.com>
2016-04-16 16:45:51 +0100

    Step 10: Making Docking Stations Release Bikes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # boris-bikes
 Week 1: Boris Bikes revision
+
+This repository has been created to practice what has been done in Week 1, and reflect on everything learnt.
+
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,10 @@
+require 'docking_station'
+
+describe DockingStation do
+
+  it { is_expected.to respond_to :release_bike}
+
+  it 'releases a working bike' do
+    expect(subject.release_bike).to be_working
+  end
+end
