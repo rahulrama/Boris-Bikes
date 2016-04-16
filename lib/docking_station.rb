bf2e0304fd184f4b37e49663145c7d03d420a7a9
rahulrama <rahulrama@gmail.com>
2016-04-16 16:58:28 +0100

    Step 11: Using Instance Variables

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,11 +2,19 @@ require_relative 'bike'
 
 class DockingStation
 
+attr_reader :bike
+
   def release_bike
     Bike.new
   end
 
+  def dock(bike)
+    @bike = bike
+  end
 
+  def bike
+    @bike
+  end
 
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,17 @@ describe DockingStation do
   it 'releases a working bike' do
     expect(subject.release_bike).to be_working
   end
+
+  it 'docks a bike' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+
+  it 'returns a docked bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
 end
